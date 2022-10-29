b842e31fb09879cc3a8c2f9dd4f4c41c67b75486
superyaz <yacamo04@hotmail.com>
2022-10-29 17:10:06 -0500

    Se crea la carpeta components

---
+++ FrontEnd/hack_day/src/components/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+function Header (){
+    return <h2>Oido pueblo como </h2>
+}
+
+/*
+class Header extends Component{
+    render(){
+        return <h2>Oido pueblo</h2>
+    }
+}
+*/
+export default Header;
+
